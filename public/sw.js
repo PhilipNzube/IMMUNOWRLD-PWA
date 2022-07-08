d300d02294d1e3fab9409253c3afd967b79a2757
PhilipNzube <72675495+PhilipNzube@users.noreply.github.com>
2022-07-08 12:07:04 +0100

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,16 @@
-let cacheData = "appV1";
+let cacheData = "MyAppCache";
 this.addEventListener("install", (event) => {
     event.waitUntil(
         caches.open(cacheData).then((cache) => {
             cache.addAll([
                 '/static/js/bundle.js',
+                '/static/js/main.7dd794ba.js',
+                '/static/css/main.ac19865a.css',
+                '/static/css/main.56bafd35.css',
+                '/manifest.json',
+                '/static/media/Background7.2ed50cea59e8cc231adf.jpg',
                 '/static/media/Background5.ee6aed90144fb9cc8818.jpg',
-                '/static/media/BloodyScreenBG.bf7d753ee003a641d083.png',
+                '/static/media/BloodyScreenBG.5772cdb53134222a9d7a.png',
                 '/static/media/Immunocalypse-icon.dceaed6ad5d9ab2c4b58.png',
                 '/static/media/GameName.0d09f05d07afcaedcada.png',
                 '/static/media/GameName2.4bfe94a4fa08b5486a03.png',
@@ -15,22 +20,29 @@ this.addEventListener("install", (event) => {
                 '/static/media/Insta.e9857db10ca57d66ad36.png',
                 '/static/media/Immunocalypse2-Icon.bdad6b135d75e6552b06.png',
                 '/Immunocalypse2-Icon.png',
+                '/Immunocalypse2-Iconb.png',
+                '/Immunocalypse2-Iconc.png',
                 '/index.html',
                 '/',
-                "/Home"
+                "/Home",
+                "/Demo"
 
             ])
         })
     )
 })
-this.addEventListener("fetch", (event) => {
-    event.respondWith(
-        caches.match(event.request).then((resp) => {
-            if (resp) {
-                return resp
-            }
-        })
+var condition=navigator.onLine?'online':'offline';
+    this.addEventListener("fetch", (event) => {
+        if(condition==='offline'){
+            console.log("Offline");
+        event.respondWith(
+            caches.match(event.request).then((resp) => {
+                if (resp) {
+                    return resp
+                }
+            })
 
 
-    )
-})
+        )
+    }
+    })
